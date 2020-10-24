225d9163bd2ddb072aa88281a7cf23107a61f004
zooRg <electroyz@mail.ru>
2020-10-24 18:39:50 +0300

    init

---
+++ src/classes/app.js
@@ -0,0 +1,25 @@
+import { Site }    from './site';
+import { Sidebar } from './sidebar';
+
+export class App
+{
+	constructor (model)
+	{
+		this.model = model;
+	}
+	
+	init ()
+	{
+		const site = new Site('#site');
+		
+		site.render(this.model);
+		
+		const update = newBlock =>
+		{
+			this.model.push(newBlock);
+			site.render(this.model);
+		};
+		
+		new Sidebar('#panel', update);
+	}
+}
